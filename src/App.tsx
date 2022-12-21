de58cba33f3737a8bf095f66a0e74f6c2cd1c33b
Kaio-Eduardo-Adorno <kaioeduardoadorno@gmail.com>
2022-12-21 19:16:18 -0300

    lint and imports fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
-import MyButton from './lib/MyButton';
+import MyButton from './lib/components/MyButton';
 
 function App() {
-    return (
-        <Wrapper>
-            <MyButton>Click me</MyButton>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <MyButton>Click me</MyButton>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-    margin: 2rem;
-    display: flex;
-    justify-content: center;
+  margin: 2rem;
+  display: flex;
+  justify-content: center;
 `;
 
 export default App;
